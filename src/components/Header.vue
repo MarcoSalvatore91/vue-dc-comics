1a66cbb5e2ba619808292677093b7744ac59954b
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-08 15:39:42 +0100

    added build and file

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Header',
+  
+}
+</script>
+
+<style scoped lang='scss'>
+
+@import '../assets/sass/utils.scss'
+
+
+</style>
