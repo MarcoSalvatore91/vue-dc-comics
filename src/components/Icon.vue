17d6bd412acb88435254b58e9f2a1a194a36ebe0
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-09 14:56:40 +0100

    added background top-jumbotron

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -2,7 +2,7 @@
   <div>
       <section id="info">
       <div v-for="link in links" :key="link.text">
-        <img :src="link.src" alt="">
+        <img :src="`../assets/img/${link.url}`" alt="">
       </div>
     </section>
   </div>
@@ -14,11 +14,11 @@ export default {
   data() {
       return {
         links: [
-          { text: "DIGITAL COMICS", src: '../assets/img/buy-comics-digital-comics.png',},
-          { text: "DC MERCHANDISE", src: "../assets/img/buy-comics-digital-merchandise.png",},
-          { text: "SUBSCRIPTION", src: "../assets/img/buy-comics-subscriptions.png",},
-          { text: "COMIC SHOP LOCATOR", src: "../assets/img/buy-comics-shop-locator.png",},
-          { text: "DC POWER VISA", src: "../assets/img/buy-dc-power-visa.svg",},
+          { text: "DIGITAL COMICS", url: 'buy-comics-digital-comics.png',},
+          { text: "DC MERCHANDISE", url: "buy-comics-digital-merchandise.png",},
+          { text: "SUBSCRIPTION", url: "buy-comics-subscriptions.png",},
+          { text: "COMIC SHOP LOCATOR", url: "buy-comics-shop-locator.png",},
+          { text: "DC POWER VISA", url: "buy-dc-power-visa.svg",},
         ]
       }
     }
---
+++ src/components/Top-Jumbotron.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <div class="top-jumbotron"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang='scss'>
+    .top-jumbotron {
+        height: 300px;
+        background-image: url('../assets/img/jumbotron.jpg');
+        background-position: top center;
+        background-size: 1500px;
+        background-repeat: no-repeat;
+    }
+</style>
